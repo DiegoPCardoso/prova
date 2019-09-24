da3ecb95d2e6388fa65b149cda63f701793282f6
Diego Cardoso <dpc1@aluno.ifnmg.edu.br>
2019-09-23 21:25:44 -0300

    commit prova

---
+++ src/Data.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IFNMG
+ */
+public class Data {
+    int dia;
+    int mes;
+    int ano;
+    void imprimir(){
+        String Data = this.dia+"/"+this.mes+"/"+this.ano;
+    }
+    
+}
---
+++ src/testaProduto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IFNMG
+ */
+public class testaProduto {
+    public static void main(String[] args){
+        Data d1=new Data();
+        d1.dia=10;
+        d1.mes=9;
+        d1.ano=2020;
+    }
+}
